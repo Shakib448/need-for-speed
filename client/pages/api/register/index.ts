58d625d33eb47f26ca63ce5990699bbe12158606
Shakib448 <shakiba448@gmail.com>
2022-09-23 12:04:56 +0600

    WIP on feat/signin

---
+++ client/pages/api/register/index.ts
@@ -0,0 +1,50 @@
+// @ts-nocheck
+import connectDB from "@server/config/db";
+import SignUpUser from "@server/models/SingIn.models";
+import {
+  userGenerateToken,
+  userRefreshToken,
+} from "@server/utils/genarateToken";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await SignUpUser.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+    } else {
+      const user = await SignUpUser.create({
+        name,
+        email,
+        password,
+      });
+
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          adminAccessToken: `Bearer ${userGenerateToken(user._id)}`,
+          refreshToken: userRefreshToken(user._id),
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+export default connectDB(handler);
